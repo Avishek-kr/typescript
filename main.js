5a231d87ee1e4756aca9dd136754b270e3d3f09a
Avishek-kr <ak251997@gmail.com>
2021-09-11 00:26:06 +0530

    var variables

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function log(message) {
-    console.log(message);
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log(i);
 }
-var message = 'Welcome avishek in typescript';
-log(message);
+doSomething();
